ce3b5947dc9aaf9d7fa93367279cd71530379a68
GriceldaTec21 <47732958+GriceldaTec21@users.noreply.github.com>
2019-12-02 14:07:05 -0800

    Update CarrotComponent.java

--- Assig7/src/prueva/CarrotComponent.java
+++ Assig7/src/prueva/CarrotComponent.java
@@ -28,51 +28,58 @@ import javax.imageio.*;
    public class CarrotComponent extends JComponent 
    { 
 	 
-	     // position of each carrot
-	     private ArrayList<Point> myPoints; 
-		 private int myX; // upper left of image x
-		 private int myY; // upper left of image y
-		 private int myDy; // change in y for gravity
-
-		 private Image carrotImage;
-		 int carrotsPoinsBunny1 = 0;// new score
-		 int carrotsPoinsBunny2 = 0;//new score
+	    
+		
+		 private int carrotsPoinsBunny1 = 0;// new score
+		 private int carrotsPoinsBunny2 = 0;//new score
                  //for bunny 2
-		 String mainPlayer = "main Player";
-		 private Image bunnyImage2, bunnyImage1;  
-		 private int my2X = getWidth()-100; // upper left of head x
-		 private int my2Y = 50; // upper left of head y
-		 int bunnynumber;
+		 private String bunnyString1 = "Gricelda 1 ", bunnyString2 = "Gricelda 2 ";
+		 
+		 private Image bunnyImage2, bunnyImage1,  carrotImage;  
+	
+		 private int bunnynumber;
 		 
 		 
-         public static final int SIZE = 800; // initial size
+         public static final int SIZE = 900; // initial size
 		 public static final int PIXELS = 50; // square size per image
 		 public static final int MOVE = 20; // keyboard move
-		 public static final int GRAVITY = 2; // gravity move
-		 public static final int CARROTS = 20; // number of carrots
+		 public static final int GRAVITY = 10; // gravity move
+		 public static final int CARROTS = 11; // number of carrots
 		 public static final int FIVE = 5;//wing 5 points
+		 
+		 // position of each carrot
+	     private ArrayList<Point> carrotPosition;  
+	     
+		 
+		 
+		 private int myX; // upper left x of bunny 
+		 private int myY ; // upper left y of bunny 
+		 private int my2X; // upper left x of bunny2
+		 private int my2Y; // upper left y of bunny2
+
 		 
 		 public static final Font FONT = new Font("Dialog", 0, 24);
 		 
 		 
 		 public CarrotComponent( ) 
 		 {
-		  setPreferredSize(new Dimension(SIZE, SIZE));
+		
+		 setPreferredSize(new Dimension(SIZE, SIZE));
+		 myX = 60;
+		 myY = 60;
+		 my2X = SIZE- 260; 
+		 my2Y = 60; 
 		 bunnyImage1 = readImage("image/bugs-bunny1.jpg");
 		 bunnyImage1 = bunnyImage1.getScaledInstance(PIXELS, PIXELS,Image.SCALE_SMOOTH);
 
 		 //second bunny
-		 setPreferredSize(new Dimension(SIZE, SIZE));
 		 bunnyImage2 = readImage("image/bunny2.jpg");
 		 bunnyImage2 = bunnyImage2.getScaledInstance(PIXELS, PIXELS,Image.SCALE_SMOOTH);
-		 
-		
-		 
-		
+
 		 carrotImage = readImage("image/carrot.gif");
 		 carrotImage = carrotImage.getScaledInstance(PIXELS, PIXELS,Image.SCALE_SMOOTH);
 		 
-		 myPoints = new ArrayList<Point>();
+		 carrotPosition = new ArrayList<Point>();
 		 }
 	 
 	 
@@ -88,8 +95,8 @@ import javax.imageio.*;
 
       // Check the current x,y of the bunny and the carrots to collect the points
       public void checkCollisions(int X, int Y, int bunny) {
-        for (int i = 0 ; i < myPoints.size( ); i++) {
-           Point point = (Point) myPoints.get(i);
+        for (int i = 0 ; i < carrotPosition.size( ); i++) {
+           Point point = (Point) carrotPosition.get(i);
        
            // if the player pass over a carrot, we remove carrot and add 5 ponts to the player 
            //who got the carrot.
@@ -104,17 +111,32 @@ import javax.imageio.*;
         	 } 
         	 
         	 
-        	 myPoints.remove(i); // removes carrot from the ArrayList
+        	 carrotPosition.remove(i); // removes carrot from the ArrayList
              i--;                          // tricky:
                                 // back i up, since we removed the ith element
               repaint( );
               }
             }
         // when there are no more carrots
-           if (myPoints.size( ) == 0) {
-        	   ImageIcon icon = new ImageIcon(bunnyImage1);
-               JOptionPane.showMessageDialog(null, "The winner is " + mainPlayer +".", 
-                 "WINNER MESSAGE", JOptionPane.INFORMATION_MESSAGE, icon);
+           if (carrotPosition.size( ) == 0) {
+        	   
+        	   
+        	   if (carrotsPoinsBunny2 > carrotsPoinsBunny1) {
+        		      ImageIcon icon = new ImageIcon(bunnyImage2);
+                     JOptionPane.showMessageDialog(null, "The winner is " + bunnyString2 +".", 
+                     "WINNER MESSAGE", JOptionPane.INFORMATION_MESSAGE, icon);
+        	   }else  if (carrotsPoinsBunny2 < carrotsPoinsBunny1) {
+        		       ImageIcon icon = new ImageIcon(bunnyImage1);
+        		       JOptionPane.showMessageDialog(null, "The winner is " + bunnyString1 +".", 
+        			   "WINNER MESSAGE", JOptionPane.INFORMATION_MESSAGE, icon);
+        	   }else {
+        		 
+    		       JOptionPane.showMessageDialog(null, "Sorry, you have not started playing. \n Place, click on the start button", 
+    			   "Message", JOptionPane.INFORMATION_MESSAGE);
+        	   }
+        		   
+        		   
+        	   
              }
        } 
 		
@@ -177,17 +199,17 @@ import javax.imageio.*;
       public void paintComponent(Graphics g) {
       
     	  
-        g.drawImage(bunnyImage2, myX, myY, PIXELS, PIXELS, null);
-        g.drawImage(bunnyImage1,my2X, my2Y, PIXELS, PIXELS, null);
+        g.drawImage(bunnyImage1, myX, myY, PIXELS, PIXELS, null);
+        g.drawImage(bunnyImage2,my2X, my2Y, PIXELS, PIXELS, null);
         
         g.setColor(Color.MAGENTA);
         g.setFont(FONT);
-        g.drawString(mainPlayer +" : " + carrotsPoinsBunny1, 10, 50);
-        g.drawString("Bug bunny : " + carrotsPoinsBunny2, getWidth()- 200, 50);
+        g.drawString(bunnyString1 +" : " + carrotsPoinsBunny1, 10, 50);
+        g.drawString(bunnyString2 +" : " + carrotsPoinsBunny2, getWidth()- 200, 50);
         
         // Draw all the carrots
-         for (int i=0; i < myPoints.size( ); i++) {
-           Point p = (Point) myPoints.get(i);
+         for (int i = 0; i < carrotPosition.size( ); i++) {
+           Point p = (Point) carrotPosition.get(i);
     
            // point.getX( ) returns a double, so we must cast to int
            g.drawImage(carrotImage,(int) (p.getX( )), (int) (p.getY( )), PIXELS, PIXELS,null);
@@ -195,21 +217,22 @@ import javax.imageio.*;
         }
     
 	
-	// Utility to read in an Image object
-	// If image cannot load, prints error output and returns null.
-	private Image readImage(String filename) {
-	    Image image = null;
+	
+	
+	private Image readImage(String upLoadimage) {
+	    // If image cannot load, prints error output and returns null.
+		Image image = null; 
 	    
 	    try {
-	      image = ImageIO.read(new File(filename));
+	      image = ImageIO.read(new File(upLoadimage));
 	   }
 	     catch (IOException e) {
-	     System.out.println("Failed to load image '" + filename + "'");
+	     System.out.println("We could find image '" + upLoadimage + "'");
 	     e.printStackTrace( );
 	  }
 	    
 	  return(image);
-	 } 
+	} 
 	
 /***
  * @param does not need it
@@ -217,15 +240,11 @@ import javax.imageio.*;
  * */
 	  public void reset(){
 		  
-		  myPoints.clear();
+		  carrotPosition.clear();
 		  for (int j = 0; j < CARROTS; j++ ){
-			  myPoints.add(randomPoint());
+			  carrotPosition.add(randomPoint());
 		  }
-		      myY =  50;
-			  myDy = 50;
-			  myX  = getWidth() / 2 - 50 ;
-			  my2X = getWidth()-100; 
-			  my2Y = 50; 
+		     
 			  carrotsPoinsBunny1 = 0;
 			  carrotsPoinsBunny2 = 0;
 			  repaint( );
